a8f8d7cde17873d2138a966f099f02dad011f477
Thomas Burleson <ThomasBurleson@Gmail.com>
2012-01-17 17:35:02 -0600

    Comment updated re> head.js

--- app/js/booter.js
+++ app/js/booter.js
@@ -3,12 +3,26 @@
 *   AngularJS - CafeTownsend
 * ******************************************************************************************************
 *
+* 
+*   This booter requires the asynchronous script loader http://headjs.com/
+*
+*   In the application (index.html), we use the following markup before </body>
+*
+*    	<script src="js/lib/head.min.js"	type="text/javascript" ></script>
+*		<script src="js/booter.js"  		type="text/javascript" ></script>
+*
+*   In the code below, we load all files in parallel but execute the javascript in sequence. 
+*   Additionally, the ready() function is called once AngularJS has executed but 
+*   before the services or controllers are executed.
+*
 * 	Copyright (c) 2011 Mindspace, LLC.
 * 	Open source under the MIT License.
 *
 */
 
 head.js ( 
+	
+	// load files in parallel but execute them in sequence
 	
 	{ jquery: 		"http://code.jquery.com/jquery-1.7.1.min.js" 	},
 	{ angular: 		"js/lib/angular.js#autobind" 					},
